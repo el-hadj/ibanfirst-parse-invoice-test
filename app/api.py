c39c5db9fe5845d637b93ef9a4625381fd65bded
el-hadj <elhadj.balde@etu.u-pec.fr>
2025-01-20 00:07:21 +0100

    [feat] adding doc string for each api

--- app/api.py
+++ app/api.py
@@ -9,6 +9,21 @@ logger = Logger("api_logs_file", "DEBUG").logger
 
 @router.get("/parse-invoice/")
 def parsing_invoices_files(file_name: Literal["invoice1", "invoice2", "invoice3"]):
+    """
+    Parses an invoice PDF file from the server's 'resources/invoices/' directory 
+    based on the provided file name.
+
+    Args:
+    -----
+        file_name (Literal["invoice1", "invoice2", "invoice3"]): The name of the invoice file 
+        to be parsed (without the '.pdf' extension). The file name must be one of 
+        'invoice1', 'invoice2', or 'invoice3'.
+
+    Returns:
+    --------
+        List[dict[]]: A list of parsed invoice information extracted from the PDF file.
+
+    """
     try:
         file_path = f"resources/invoices/{file_name}.pdf"
         extracted_text = extract_text_from_pdf(file_path, logger)
@@ -26,6 +41,18 @@ def parsing_invoices_files(file_name: Literal["invoice1", "invoice2", "invoice3"
 
 @router.post("/parse-invoice/upload/")
 async def parsing_invoices_files(file: UploadFile = File(...)):
+    """
+    Parses an uploaded invoice PDF file and extracts the relevant invoice details.
+
+    Args:
+    -----
+        file (UploadFile): The invoice PDF file uploaded by the user.
+
+    Returns:
+    -------
+        List[dict]: A list of parsed invoice information extracted from the uploaded file.
+    
+    """
     try:
 
         contents = await file.read()
